0523254a055eb3c3b5d4f412e171bc25c6baf52a
alinaseem123 <alinaseem21102002@gmail.com>
2026-01-19 13:52:30 +0500

    fix(security): update CORS origin to allow Vercel production deploy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://my-ecommerce-website-1w02w5xgi-ali-naseems-projects-305588ff.vercel.app',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
